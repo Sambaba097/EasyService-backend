2e998f9ce73174007b4e1e72efd050c235eb192a
samba ba <ba269962@gmail.com>
2025-03-13 04:04:50 +0000

    inscription et authentification des utilisateurs avec leur différends roles

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const mongoose = require('mongoose');
+const PORT = process.env.PORT || 3000;
+require('dotenv').config();
+const authRoutes = require('./routes/authRoutes');
+
+
+
+// Middleware
+app.use(express.json()); // permet de lire le JSON dans les requetes
+app.use(express.urlencoded({ extended: true })); // permet de lire le JSON dans les requetes
+
+app.use(cors()); // permet d'accéder à l'API depuis le frontend
+
+// connexion à MongoDB
+
+mongoose.connect(process.env.MONGO_URI)
+.then(() => console.log('Connexion à MongoDB réussie !'))
+ .catch((err) => console.error('Erreur de connexion à MongoDB :', err));
+
+ // Route de base pour tester l'API
+app.get('/', (req, res) => {
+    res.send('Bienvenue sur l\'API de gestion de service !');
+});
+
+// Routes d'authentification
+app.use('/api/auth', authRoutes);
+
+// Démarrer le serveur
+
+app.listen(PORT, () => console.log(`L'API est démarrée sur le port ${PORT}`));
---
+++ controllers/authController.js
@@ -0,0 +1,79 @@
+const User = require('../models/User');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+
+// Inscription
+exports.register = async (req, res) => {
+
+  try {
+    const { nom, prenom, email, password, role } = req.body;
+
+    console.log(req.body);
+    // Vérifier que le mot de passe est bien fourni
+    if (!password) {
+        return res.status(400).json({ message: 'Le mot de passe est requis' });
+    }
+    // Vérifier si l'utilisateur existe déjà
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({ message: 'Cet email est déjà utilisé.' });
+    }
+
+    // Créer un nouvel utilisateur
+    const user = new User({ nom, prenom, email, password, role });
+    await user.save();
+
+    // Générer un token JWT
+    const token = jwt.sign({ userId: user._id, role: user.role, email: user.email, prenom: user.prenom}, process.env.JWT_SECRET , { expiresIn: '1h' });
+
+    res.status(201).json({ message: 'Utilisateur créé avec succès', token, 
+        user:{
+            nom: user.nom,
+            prenom: user.prenom,
+            email: user.email,
+            role: user.role,
+        }
+     });
+  } catch (err) {
+    if (err.name === 'ValidationError') {
+        return res.status(400).json({ message: 'Données invalides: Erreur lors de la création de l\'utilisateur', error: err.message });
+    }
+    res.status(500).json({ message: 'Erreur lors de l\'inscription.' });
+  }
+};
+
+// Connexion
+exports.login = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    // Vérifier que email et password sont fournis
+    if (!email || !password) {
+        return res.status(400).json({ message: 'Email et mot de passe requis' });
+    }
+    // Vérifier si l'utilisateur existe
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ message: 'Utilisateur non trouvé' });
+    }
+
+    // Vérifier le mot de passe
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+      return res.status(401).json({ message: 'Mot de passe incorrect' });
+    }
+
+    // Générer un token JWT
+    const token = jwt.sign({ userId: user._id, role: user.role, email: user.email, prenom: user.prenom }, process.env.JWT_SECRET, { expiresIn: '1h' });
+    // Renvoyer également les informations de l'utilisateur
+    res.status(200).json({message: 'Connexion reussi!', token, 
+
+        user: {
+            email: user.email,
+            prenom: user.prenom
+        }
+     });
+  } catch (err) {
+    res.status(500).json({ message: 'Erreur lors de la connexion.' });
+  }
+};
